3788d3978634f915e1ed269c3999f5869d1ea8b3
maki.sekine <ec2-user@ip-172-31-19-36.us-east-2.compute.internal>
2018-05-18 06:31:34 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,6 +36,10 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -66,6 +70,10 @@ class TasksController extends Controller
     // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->save();
