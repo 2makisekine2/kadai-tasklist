e9cf86081a8b9e975a77841866b6425e3d58303f
maki.sekine <ec2-user@ip-172-31-19-36.us-east-2.compute.internal>
2018-05-18 05:50:50 +0000

    composer update&fine adjustment

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,7 @@
 
     <h1>タスク一覧</h1>
 
-        @if (count($tasks) > 0)
+    @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
                 <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
@@ -12,6 +12,6 @@
         </ul>
     @endif
     
-    {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+    {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
     
 @endsection
