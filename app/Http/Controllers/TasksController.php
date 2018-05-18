bb2a4c7cd5edaa678df40e57a369829064be0335
maki.sekine <ec2-user@ip-172-31-19-36.us-east-2.compute.internal>
2018-05-18 06:43:45 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,11 +36,13 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
-        $this->validate($request, [
+            $this->validate($request, [
+            'status' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -71,10 +73,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
